6560b1d0279f46340c789776a94f749b33c53ede
ml-okuda <shuhei.okuda@monstar-lab.com>
2022-02-22 18:44:25 +0900

    Update function to return Promise

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -11,7 +11,7 @@ class Memo {
   constructor() {
   }
 
-  async open() {
+  open() {
     return new Promise((resolve, reject) => {
       this.db = new sqlite3.Database("./test.db");
       this.db.serialize(() => {
@@ -21,7 +21,7 @@ class Memo {
     });
   }
 
-  async all() {
+  all() {
     return new Promise((resolve, reject) => {
       this.db.all("select * from memos", async (err, rows) => {
         if (err) reject(err);
@@ -31,15 +31,21 @@ class Memo {
   }
 
   insert(title, content) {
-    this.db.run("insert into memos(title, content) values(?, ?)", title, content);
+    return new Promise((resolve, reject) => {
+      this.db.run("insert into memos(title, content) values(?, ?)", title, content);
+    });
   }
 
   delete(id) {
-    this.db.run("delete from memos where id=?", id);
+    return new Promise((resolve, reject) => {
+      this.db.run("delete from memos where id=?", id);
+    });
   }
 
   close() {
-    this.db.close();
+    return new Promise((resolve, reject) => {
+      this.db.close();
+    });
   }
 }
 
@@ -63,13 +69,13 @@ class Memo {
       input: process.stdin
     });
 
-    reader.on("line", (line) => {
+    await reader.on("line", (line) => {
       lines.push(line);
     });
-    reader.on("close", () => {
-      memo.insert(lines[0], lines);
+    await reader.on("close", async () => {
+      await memo.insert(lines[0], lines);
     })
   }
 
-  memo.close();
+  //await memo.close();
 })();
