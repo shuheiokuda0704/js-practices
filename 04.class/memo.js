cf749258eeb1e673f93391a3544cb115ef08efd4
ml-okuda <shuhei.okuda@monstar-lab.com>
2022-02-28 06:20:12 +0900

    Fix eslint detected warnings/errors

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,148 +1,144 @@
 #!/usr/bin/env node
 
-const Sqlite3 = require("sqlite3");
-const { prompt } = require("enquirer");
+const Sqlite3 = require('sqlite3')
+const { prompt } = require('enquirer')
 
-const argv = require('minimist')(process.argv.slice(2));
-const list_option = argv.l || false;
-const reference_option = argv.r || false;
-const delete_option = argv.d || false;
-
+const argv = require('minimist')(process.argv.slice(2))
+const listOption = argv.l || false
+const referenceOption = argv.r || false
+const deleteOption = argv.d || false
 
 class MemoDB {
-  constructor() {
-  }
-
-  open() {
+  open () {
     return new Promise((resolve, reject) => {
-      this.db = new Sqlite3.Database("./test.db");
+      this.db = new Sqlite3.Database('./test.db')
       this.db.serialize(() => {
-        this.db.run("create table if not exists memos(id INTEGER PRIMARY KEY AUTOINCREMENT, title, content)");
-      });
-      resolve(true);
-    });
+        this.db.run('create table if not exists memos(id INTEGER PRIMARY KEY AUTOINCREMENT, title, content')
+      })
+      resolve(true)
+    })
   }
 
-  all() {
+  all () {
     return new Promise((resolve, reject) => {
-      this.db.all("select * from memos", async (err, rows) => {
-        if (err) reject(err);
-        resolve(rows);
-      });
-    });
+      this.db.all('select * from memos', async (err, rows) => {
+        if (err) reject(err)
+        resolve(rows)
+      })
+    })
   }
 
-  insert(title, content) {
+  insert (title, content) {
     return new Promise((resolve, reject) => {
-      this.db.run("insert into memos(title, content) values(?, ?)", title, content);
-    });
+      this.db.run('insert into memos(title, content) values(?, ?)', title, content)
+    })
   }
 
-  delete(id) {
+  delete (id) {
     return new Promise((resolve, reject) => {
-      this.db.run("delete from memos where id=?", id);
-    });
+      this.db.run('delete from memos where id=?', id)
+    })
   }
 
-  close() {
+  close () {
     return new Promise((resolve, reject) => {
-      this.db.close();
-    });
+      this.db.close()
+    })
   }
 }
 
 class Memo {
-  constructor() {
-    this.memoDb = new MemoDB();
+  constructor () {
+    this.memoDb = new MemoDB()
   }
 
-  async list() {
-    await this.memoDb.open();
+  async list () {
+    await this.memoDb.open()
 
-    const memos = await this.memoDb.all();
-    memos.forEach(memo => console.log(memo.title));
-    this.memoDb.close();
+    const memos = await this.memoDb.all()
+    memos.forEach(memo => console.log(memo.title))
+    this.memoDb.close()
 
-    return memos;
+    return memos
   }
 
-  async reference() {
-    await this.memoDb.open();
-    const memos = await this.memoDb.all();
+  async reference () {
+    await this.memoDb.open()
+    const memos = await this.memoDb.all()
 
-    if (memos.length === 0) return;
+    if (memos.length === 0) return
 
     const question = [{
       type: 'select',
       name: 'memo',
       message: 'Choose a note you want to see:',
-      choices: memos.map(function(memo) {
-        return {id: memo.id, title: memo.title, content: memo.content}
+      choices: memos.map(function (memo) {
+        return { id: memo.id, title: memo.title, content: memo.content }
       }),
-      result() {
-        return {id: this.focused.id, title: this.focused.name, content: this.focused.content};
+      result () {
+        return { id: this.focused.id, title: this.focused.name, content: this.focused.content }
       }
-    }];
+    }]
 
-    const answer = await prompt(question);
-    console.log(answer.memo.content);
-    this.memoDb.close();
+    const answer = await prompt(question)
+    console.log(answer.memo.content)
+    this.memoDb.close()
   }
 
-  async delete() {
-    await this.memoDb.open();
-    const memos = await this.memoDb.all();
+  async delete () {
+    await this.memoDb.open()
+    const memos = await this.memoDb.all()
 
-    if (memos.length === 0) return;
+    if (memos.length === 0) return
 
     const question = [{
       type: 'select',
       name: 'memo',
       message: 'Choose a note you want to delete:',
-      choices: memos.map(function(memo) {
-        return {id: memo.id, title: memo.title}
+      choices: memos.map(function (memo) {
+        return { id: memo.id, title: memo.title }
       }),
-      result() {
-        return {id: this.focused.id, title: this.focused.name};
+      result () {
+        return { id: this.focused.id, title: this.focused.name }
       }
-    }];
+    }]
 
-    const answer = await prompt(question);
-    await this.memoDb.delete(answer.memo.id);
-    this.memoDb.close();
+    const answer = await prompt(question)
+    await this.memoDb.delete(answer.memo.id)
+    this.memoDb.close()
   }
 
-  async insert() {
-    await this.memoDb.open();
+  async insert () {
+    await this.memoDb.open()
 
-    process.stdin.resume();
-    process.stdin.setEncoding('utf8');
+    process.stdin.resume()
+    process.stdin.setEncoding('utf8')
 
-    var lines = [];
-    var reader = require("readline").createInterface({
+    const lines = []
+    const reader = require('readline').createInterface({
       input: process.stdin
-    });
+    })
 
-    await reader.on("line", (line) => {
-      lines.push(line);
-    });
-    await reader.on("close", async () => {
-      await this.memoDb.insert(lines[0], lines.join('\n'));
-      this.memoDb.close();
+    await reader.on('line', (line) => {
+      lines.push(line)
+    })
+    await reader.on('close', async () => {
+      await this.memoDb.insert(lines[0], lines.join('\n'))
+      this.memoDb.close()
     })
   }
 }
 
-(() => { 
-  memo = new Memo();
+(() => {
+  const memo = new Memo()
 
-  if (list_option) {
-    memo.list();
-  } else if (reference_option) {
-    memo.reference();
-  } else if (delete_option) {
-   memo.delete();
+  if (listOption) {
+    memo.list()
+  } else if (referenceOption) {
+    memo.reference()
+  } else if (deleteOption) {
+    memo.delete()
   } else {
-    memo.insert();
+    memo.insert()
   }
-})();
+})()
